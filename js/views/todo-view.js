db6e6580378322b2f14d76ebfbb0d839d8787532
Mark Matthews <mlmatth1@gmail.com>
2018-08-20 14:33:16 -0700

    Added new Priority Feature

--- js/views/todo-view.js
+++ js/views/todo-view.js
@@ -21,6 +21,7 @@ var app = app || {};
 			//'dblclick label': 'edit',
 			'click .destroy': 'clear',
 			'click .edit-btn': 'edit',
+			'click .priority-btn': 'togglePriority',
 			'keypress .edit': 'updateOnEnter',
 			'keydown .edit': 'revertOnEscape',
 			'blur .edit': 'close'
@@ -52,8 +53,10 @@ var app = app || {};
 
 			this.$el.html(this.template(this.model.toJSON()));
 			this.$el.toggleClass('completed', this.model.get('completed'));
+			this.$el.toggleClass('priority', this.model.get('priority'));
 			this.toggleVisible();
 			this.$input = this.$('.edit');
+
 			return this;
 		},
 
@@ -65,6 +68,11 @@ var app = app || {};
 			return this.model.get('completed') ?
 				app.TodoFilter === 'active' :
 				app.TodoFilter === 'completed';
+		},
+
+		// Toggle the '"priority"' state of the model.
+		togglePriority: function () {
+			this.model.togglePriority();
 		},
 
 		// Toggle the `"completed"` state of the model.
